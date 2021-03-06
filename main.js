8a1f10d7c61018fe8646180e9e19eff9cb33f810
Richard Robinson <rir@my.yorku.ca>
2021-03-06 00:11:43 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const API_URL = 'https://api.rr.codes'
+
+function mapValues(object, transformation) {
+    return Object.entries(object).reduce((prev, [key, value]) => {
+        prev[key] = typeof value === 'object' ? mapValues(value, transformation) : transformation(value);
+        return prev;
+    }, {})
+}
+
+/**
+ * Parses the response by formatting the JSON into a prettified HTML tag
+ * @param {{email: string, links: Record<string, string>, ...}} response
+ * @return string
+ */
+function parseResponse(response) {
+   // const mappedObject = mapValues(response, (value) => `<span class="json-value">${value}</span>`)
+
+    const links = mapValues(response.links, (value) => `<a href=${value}>${value}</a>`)
+
+    const email = `<a href=mailto:${response.email}?subject=Hey!>${response.email}</a>`
+
+    const parsedString = JSON.stringify({
+        ...response,
+        links,
+        email
+    }, null, 4);
+
+    console.log(parsedString)
+
+    return `<pre><code>${parsedString}</code></pre>`
+}
+
+
+async function onLoad() {
+    const aboutResponse = await fetch(`${API_URL}/about`)
+    const body = await aboutResponse.json();
+
+    document.getElementById("about-response").innerHTML = parseResponse(body);
+
+    // noinspection JSUnresolvedVariable
+    hljs.highlightAll()
+}
+
+(async () => await onLoad())()
